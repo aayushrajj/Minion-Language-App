856e66ea803ea4c46b98fd39361ebc0c1b0fcac8
aayushrajj <ayushraj0309@outlook.com>
2022-06-03 23:56:35 +0530

    update : added button
    added button in html file and created a reference to this button in app.js file

---
+++ app.js
@@ -0,0 +1,14 @@
+// console.log("A seperate JS file used");
+
+// //input
+// var username = prompt("Give me your username");
+
+// //processing
+// var welcomeMessage = "This script works! " + username;
+
+// //output
+// alert(welcomeMessage);
+
+var btntranslate = document.querySelector("#btn-translate");
+
+console.log(btntranslate);
