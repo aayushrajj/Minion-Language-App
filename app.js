6e0b1b436614105cc58b4deae31d389dfff2124f
aayushrajj <ayushraj0309@outlook.com>
2022-06-04 23:36:53 +0530

    update : input output processing
    take input and show it as an output by using JS

--- app.js
+++ app.js
@@ -9,15 +9,41 @@
 // //output
 // alert(welcomeMessage);
 
+
+// // The querySelector() method returns the first element that matches a CSS selector.
+// document.querySelector("text-area");
+
+// // this way we select first occuring class with same name
+// .btn-primary 
+
+// // this way we select first occuring id with same name
+// #input-btn
+
+// //  this way we select 'input' element with an attribute name='translator' ; [] is attribute selector
+// document.querySelector("input[name='translator']");
+
+
+
 var btntranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
+var outputDiv = document.querySelector("#output");
 
-console.log(textInput);
+
+// outputDiv.innerText = "Ayush Raj";
+// console.log(outputDiv);
+
+// console.log(textInput);
+
+// function clickHandler(){
+//     console.log("Clicked!");
+//     console.log("Input--> " , textInput.value);
+// }
 
 function clickHandler(){
-    console.log("Clicked!");
-    console.log("input" , textInput.value);
+    outputDiv.innerText = "sn&&fui##22@@!!  " + textInput.value; 
 }
 
 btntranslate.addEventListener("click" , clickHandler);
+
+
 
