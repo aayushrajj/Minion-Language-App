ed7997d59700437ab2cea51855d9bd6612c3d2d6
aayushrajj <ayushraj0309@outlook.com>
2022-06-07 15:28:14 +0530

    update : comments added
    --> comments added in files
    --> social links added in footer

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 
-
+// first we fetched all the variables ( INPUT , OUTPUT , BUTTON ) required ahead 
 var btntranslate = document.querySelector("#btn-translate");
 var textInput = document.querySelector("#text-input");
 var outputDiv = document.querySelector("#output");
 
 
-
+// we fetched the server url , to which we want to talk or get our data
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
+
+// then we defined a function for whenever we want to convert a text or use server URL for the text
 function getTranslationURL(text){
     var encodedText = encodeURI(text);
     return serverURL + "?" + "text=" + encodedText
 }
 
+// there can be error sometimes , so a function for handling errors
 function errorHandler(error){
     console.log("error occured",error);
     alert("Too Many Requests! Rate limit of 5 requests per hour exceeded. Please try again after sometime");
 }
 
+
+// what to do when button clicks happens..
 function clickHandler(){
-    // taking input
+    // read the input
     var input = textInput.value; 
 
+    // calling server for processing
     var finalURL = getTranslationURL(input);
     
-    // calling server for processing
+    // go fetch the response we got from the server --> then convert it to json --> 
+    // then  get that translated text and show this as the main output
     fetch(finalURL)
         .then( response => response.json() )
         .then(json => {
@@ -35,6 +42,8 @@ function clickHandler(){
 
 }
 
+
+// what to do when a click happens...
 btntranslate.addEventListener("click" , clickHandler);
 
 
